5cfa7286d0c265c9fb5bd5ec61abc5a1df93631b
juliatangerino <julianatangerino2017@gmail.com>
2021-07-09 01:25:44 -0300

    Alterações no arquivo recebe_formulario

--- formulario.php
+++ formulario.php
@@ -1,5 +1,9 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
@@ -8,6 +12,10 @@
     <title>Formulário com php</title>
     <link rel="stylesheet" href="./assets/css/tailwind.css">
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 </head>
 
 <body class="bg-green-50">
@@ -16,76 +24,64 @@
 
         <div class="grid grid-cols-5 shadow-2xl">
 
-            <div class="col-span-5 md:col-span-2 lg:block lg:w-12/12 w-full  h-auto bg-green-500 hidden bg-cover rounded-l-lg">
-                <img src="./assets/img/Programming.gif" alt="" class="mt-60">
+            <div class="col-span-5 md:col-span-2 md:flex justify-items-center md:rounded-l-lg items-center lg:w-12/12 w-full h-full bg-green-500 bg-cover rounded-t-lg">
+                    <img src="./assets/img/Programming.gif" alt="" class="p-4">
             </div>
 
-            <div class="col-span-5 md:col-span-3 lg:block lg:w-12/12 w-full h-auto bg-gray-50 hidden bg-cover rounded-r-lg p-14">
+            <div class="col-span-5 md:col-span-3 lg:block lg:w-12/12 w-full h-auto bg-gray-50 bg-cover rounded-r-lg p-14">
 
                 <form action="recebe_formulario.php" method="post">
 
                     <div class="grid gap-y-3">
 
                         <label for="">
-                            <ul for="">
-                                <p class="text-gray-600 font-semibold py-1">Nome:</p>
-                                <input type="text" name="nome" id="nome" class="bg-gray-200 py-1 w-full rounded-lg border border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent font-light text-gray-700 placeholder-gray-400 pl-4" placeholder="Digite o seu nome..." />
-                            </ul>
+                            <p class="text-gray-600 font-semibold py-1">Nome:</p>
+                            <input type="text" name="nome" id="nome" class="bg-gray-200 py-1 w-full rounded-lg border border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent font-light text-gray-700 placeholder-gray-400 pl-4" placeholder="Digite o seu nome..." />
                         </label>
 
                         <label for="">
-                            <ul for="">
-                                <p class="text-gray-600 font-semibold py-1">E-mail:</p>
-                                <input type="text" name="email" id="nome" class="bg-gray-200 py-1 w-full rounded-lg border border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent font-light text-gray-700 placeholder-gray-400 pl-4" placeholder="Digite o seu e-mail..." />
-                            </ul>
+                            <p class="text-gray-600 font-semibold py-1">E-mail:</p>
+                            <input type="text" name="email" id="email" class="bg-gray-200 py-1 w-full rounded-lg border border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent font-light text-gray-700 placeholder-gray-400 pl-4" placeholder="Digite o seu e-mail..." />
                         </label>
 
 
                         <label for="">
-                            <ul for="">
-                                <p class="text-gray-600 font-semibold py-1">Telefone:</p>
-                                <input type="telefone" name="email" id="nome" class="bg-gray-200 py-1 w-full rounded-lg border border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent font-light text-gray-700 placeholder-gray-400 pl-4" placeholder="Digite o seu telefone..." />
-                            </ul>
+                            <p class="text-gray-600 font-semibold py-1">Telefone:</p>
+                            <input type="telefone" name="telefone" id="telefone" class="bg-gray-200 py-1 w-full rounded-lg border border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent font-light text-gray-700 placeholder-gray-400 pl-4" placeholder="Digite o seu telefone..." />
                         </label>
 
 
                         <label for="">
-                            <ul for="">
-                                <p class="text-gray-600 font-semibold py-1">Celular:</p>
-                                <input type="celular" name="email" id="nome" class="bg-gray-200 py-1 w-full rounded-lg border border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent font-light text-gray-700 placeholder-gray-400 pl-4" placeholder="Digite o seu celular..." />
-                            </ul>
-
+                            <p class="text-gray-600 font-semibold py-1">Celular:</p>
+                            <input type="celular" name="celular" id="celular" class="bg-gray-200 py-1 w-full rounded-lg border border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent font-light text-gray-700 placeholder-gray-400 pl-4" placeholder="Digite o seu celular..." />
                         </label>
-
                     </div>
 
 
                     <div class="grid gap-y-4 mt-6">
 
-                        <label>
-                            <p class="text-gray-600 font-semibold py-1"> Qual sua área de atuação?</p>
-                            <ul>
-                                <input type="checkbox" class="checked:bg-blue-600 checked:border-transparent">
-                                <span>Front-end</span>
-                            </ul>
-                            <ul>
-                                <input type="checkbox" class="checked:bg-blue-600 checked:border-transparent">
-                                <span>Back-end</span>
-                            </ul>
-                            <ul>
-                                <input type="checkbox" class="checked:bg-blue-600 checked:border-transparent">
-                                <span>Full Stack</span>
-                            </ul>
-                        </label>
+                        <p class="text-gray-600 font-semibold py-1"> Qual sua área de atuação?</p>
 
+                        <label>
+                            <input type="checkbox" name="atuacao[]" value="front-end">
+                            <span>Front-end</span>
+                        </label>
+                        <label>
+                            <input type="checkbox" name="atuacao[]" value="back-end">
+                            <span>Back-end</span>
+                        </label>
+                        <label>
+                            <input type="checkbox" name="atuacao[]" value="fullstack">
+                            <span>Full Stack</span>
+                        </label>
 
                         <label for="">
                             <p class="text-gray-600 font-semibold py-1">Por onde nos conheceu?</p>
-                            <select name="onde_conheceu" id="" class="bg-gray-200 py-2 px-3 w-full rounded-lg border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent font-light text-gray-700 placeholder-gray-300">
+                            <select name="ondeConheceu" id="" class="bg-gray-200 py-2 px-3 w-full rounded-lg border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:border-transparent font-light text-gray-700 placeholder-gray-300">
                                 <option value="">Selecione</option>
-                                <option value="">Google</option>
-                                <option value="">Amigos</option>
-                                <option value="">Tv</option>
+                                <option value="google">Google</option>
+                                <option value="amigos">Amigos</option>
+                                <option value="tv">Tv</option>
                             </select>
                         </label>
 
@@ -95,40 +91,28 @@
                         </label>
 
 
-
-
-                        <label for="">
-                            <p class="text-gray-600 font-semibold py-1">Você gostaria de trabalhar:</p>
-                            <ul for="">
-                                <input type="radio" name="option" id=""> Presencialmente
-                            </ul>
-
-                            <ul for="">
-                                <input type="radio" name="option" id=""> Remoto
-                            </ul>
-
-                            <ul for="">
-                                <input type="radio" name="option" id=""> Não tenho preferencia
-                            </ul>
+                        <p class="text-gray-600 font-semibold py-1">Você gostaria de trabalhar:</p>
+                        <label>
+                            <input type="radio" name="option" value="presencialmente"> Presencialmente
                         </label>
+
+                        <label>
+                            <input type="radio" name="option" value="remoto"> Remoto
+                        </label>
+
+                        <label>
+                            <input type="radio" name="option" value="sempreferencia"> Não tenho preferencia
+                        </label>
+
 
                         <div class="grid justify-items-end">
                             <button type="submit" class="py-3 px-8 text-white font-bold bg-green-500 rounded-lg"> Me candidatar </button>
                         </div>
 
-
-
                     </div>
-
                 </form>
-
-
             </div>
-
-
-
         </div>
-
 </body>
 
 </html>
--- recebe_formulario.php
+++ recebe_formulario.php
@@ -1,7 +1,49 @@
-//O que vai ligar o meu formulário ao meu PHP é o name de cada INPUT
-
 <?php
 
-var_dump( $_POST );
+/* O que vai ligar o meu formulário ao meu PHP é o name de cada INPUT */
+
+$nome = trim($_POST['nome']);
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$celular = $_POST['celular'];
+
+if (is_numeric($celular) && !empty($celular)) {
+    echo "<strong>Celular:</strong>" . $celular . "<br>";
+} else {
+    echo "<strong>Celular:</strong> Preencha o campo de contato! <br>";
+}
+
+if (is_numeric($telefone) && !empty($telefone)) {
+    echo "<strong>Telefone:</strong>" . $telefone . "<br>";
+} else {
+    echo "<strong>Telefone:</strong> Preencha o campo de contato! <br>";
+}
+
+$atuacoes = $_POST['atuacao'] ?? null;
+$ondeConheceu = $_POST['ondeConheceu'];
+$mensagem = strip_tags( $_POST['mensagem']);
+$option = $_POST['option'];
+
+if (empty($nome)) {
+    echo "Nome não preenchido <br>"; 
+} else {
+    echo "<strong>Nome:</strong>" . $nome . "<br>";
+}
+
+echo "<strong>Email:</strong>" . $email . "<br>";
+
+
+    if (is_null($atuacoes)) {
+        echo "<strong>Atuação:</strong> Nenhuma área de atuação foi preenchida <br>";
+    } else {
+
+        $areaDeAtuacao = implode(", ", $atuacoes);
+        echo "<strong>Atuação:</strong>" . $areaDeAtuacao . "<br>";
+
+    }
+
+echo "<strong>Por onde nos conheceu:</strong>" . $ondeConheceu . "<br>";
+echo "<strong>Conte um pouco mais sobre você:</strong>" . $mensagem . "<br>";
+echo "<strong>Você gostaria de trabalhar:</strong>" . $option . "<br>";
 
 ?>
